1ff8f8859eda80ef8a41c78bab3d28e7a31037cd
Avalyn Baldyga <jackedak@gmail.com>
2025-04-13 00:37:18 -0400

    save

--- src/train.py
+++ src/train.py
@@ -5,5 +5,9 @@ data = json.load(open("pm.json"))
 model = sentience.create_model()
 
 data = list(filter(lambda x: len(x["msg"])+len(x["ref"]) < 256, data))
-for x in data:
-    sentience.train(model, x["ref"], x["msg"])
+try:
+    for x in data:
+        sentience.train(model, x["ref"], x["msg"])
+except:
+    model.save("model.keras")
+model.save("model.keras")
